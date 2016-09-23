0868d4b3bb03d017ad326e53590e26bafff59d48
Winnie Liang <wliang6@buffalo.edu>
2016-09-23 00:19:35 -0400

    Completed a simple calculator program!

---
+++ Fees.java
@@ -0,0 +1,72 @@
+package UBTuitionCalculator;
+
+/**
+ * ProjectPortfolio
+ * @author Winnie Sep 22, 2016
+ * Fees.java
+ * The purpose of this small summer tuition calculator is to implement 
+ * the MVC (Model-View-Controller) architectural pattern. 
+ * Model - transitioning the data to object classes and making a UML diagram to show the design of the UI
+ * View - what the user visualizes; particularly the user interface (UI)
+ * Controller - the code and logic behind the program; links the model and the view together
+ * 
+ * The interface allows a typical undergraduate student from University at Buffalo
+ * to input their name, campus status, and # courses they are taking. 
+ */
+
+
+//*******MY MODEL********
+public class Fees { 
+	
+	final static double NYS_TUITION= 270.00;
+	final static double NON_NYS_TUITION = 898.00;
+	final static double COMPREHENSIVE_FEE = 74.06;
+	final static double ACADEMIC_EXCELLENCE_FEE = 15.63;
+	final static double ACTIVITY_FEE = 17.52;
+	final static double TRANSCRIPT_FEE = 5.00;
+	
+	
+	private static String name;
+	private static boolean nysResident;
+	private static int credits; //how many credits they are taking
+	
+	public Fees(String nam, boolean resident, int credit){
+		name = setName(nam);
+		nysResident = resident;
+		credits = setCredits(credit);
+	}
+
+
+	private String setName(String name) {
+		name = name.toLowerCase();
+		name = name.substring(0, 1).toUpperCase() + name.substring(1);
+		return name;
+		
+	}
+	
+	private int setCredits(int credit) {
+		if(credit < 0){
+			credit = 0;
+		}
+		return credit;
+	}
+	
+	public static double calculateFee(){
+		double total = 0.0;
+		if(nysResident){
+			if(credits < 1){
+				total = 0.0;
+			}else{
+				total = (NYS_TUITION + COMPREHENSIVE_FEE + ACADEMIC_EXCELLENCE_FEE) * credits + ACTIVITY_FEE + TRANSCRIPT_FEE;
+			}
+		} else{
+			if(credits < 1){
+				total = 0.0;
+			}else{
+				total = (NON_NYS_TUITION + COMPREHENSIVE_FEE + ACADEMIC_EXCELLENCE_FEE) * credits + ACTIVITY_FEE + TRANSCRIPT_FEE;
+			}
+		}
+		return total;
+		
+	}
+}
---
+++ UInterface.java
@@ -0,0 +1,98 @@
+package UBTuitionCalculator;
+
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+
+/**
+ * ProjectPortfolio
+ * @author Winnie Sep 22, 2016
+ * Fees.java
+ * The purpose of this small summer tuition calculator is to implement 
+ * the MVC (Model-View-Controller) architectural pattern. 
+ * Model - transitioning the data to object classes and making a UML diagram to show the design of the UI
+ * View - what the user visualizes; particularly the user interface (UI)
+ * Controller - the code and logic behind the program; links the model and the view together
+ * 
+ * The interface allows a typical undergraduate student from University at Buffalo
+ * to input their name, campus status, and # courses they are taking. 
+ */
+
+
+public class UInterface extends JFrame implements ActionListener{
+	//*******MY INTERFACE********
+	JLabel lblLastName = new JLabel("Enter your last name: ");
+	JTextField txtLastName = new JTextField(10);
+	JRadioButton radNYS = new JRadioButton("NYS Resident", false);
+	JRadioButton radNONNYS = new JRadioButton("NonNYS Resident", false);
+	ButtonGroup butStatus = new ButtonGroup();
+	JLabel lblCredits = new JLabel("Enter the number of credits you are taking: ");
+	JTextField txtCredits = new JTextField(10);
+	JButton butSubmit = new JButton("Submit details");
+	JLabel lblTotalFees = new JLabel("The total fees came up to be: ");
+	JTextField txtTotalFees = new JTextField(10);
+	JButton butClear = new JButton("Clear fields");
+	
+	public UInterface(){
+		setTitle("Summer Tuition Calculator");
+		setLayout(new FlowLayout());
+		setSize(300, 220); //w, h
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		
+		butStatus.add(radNYS);
+		butStatus.add(radNONNYS);
+		add(lblLastName);
+		add(txtLastName);
+		add(radNYS);
+		add(radNONNYS);
+		add(lblCredits);
+		add(txtCredits);
+		add(butSubmit);
+		add(lblTotalFees);
+		add(txtTotalFees);
+		add(butClear);
+		butSubmit.addActionListener(this);
+		butClear.addActionListener(this);
+		
+	}
+	
+
+	
+	//*******MY CONTROLLER********
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == butSubmit){
+			DecimalFormat formatDollars = new DecimalFormat("$###,##0.00");
+			String name = txtLastName.getText();
+			boolean nysResident = radNYS.isSelected();
+			if(txtCredits.getText().length() > 0){
+				int credits = Integer.parseInt(txtCredits.getText());
+				Fees fees = new Fees(name, nysResident, credits);
+				double totalFees = fees.calculateFee();
+				txtTotalFees.setText(formatDollars.format(totalFees));
+			}
+		}
+		if(e.getSource() == butClear){
+			txtLastName.setText("");
+			txtCredits.setText("");
+			txtTotalFees.setText("");;
+		}
+		
+	}
+	
+	public static void main(String[] args){
+		UInterface calculateFeeUI = new UInterface();
+		calculateFeeUI.setVisible(true);
+	}
+	
+}

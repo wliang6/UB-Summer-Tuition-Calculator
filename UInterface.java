1adfc805a9c81d1a37f08b1ee0268b46b04171f5
Winnie Liang <wliang6@buffalo.edu>
2016-09-23 00:41:25 -0400

    Updated comments above.

--- UInterface.java
+++ UInterface.java
@@ -16,7 +16,7 @@ import javax.swing.JTextField;
 /**
  * ProjectPortfolio
  * @author Winnie Sep 22, 2016
- * Fees.java
+ * UInterface.java
  * The purpose of this small summer tuition calculator is to implement 
  * the MVC (Model-View-Controller) architectural pattern. 
  * Model - transitioning the data to object classes and making a UML diagram to show the design of the UI
@@ -24,7 +24,8 @@ import javax.swing.JTextField;
  * Controller - the code and logic behind the program; links the model and the view together
  * 
  * The interface allows a typical undergraduate student from University at Buffalo
- * to input their name, campus status, and # courses they are taking. 
+ * to input their name, resident status, and # credits they are taking. 
+ * The estimated cost is then displayed to them on the interface. 
  */
 
 
